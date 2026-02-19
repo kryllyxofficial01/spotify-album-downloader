002f3d285758e3ea0aa73af921fc43ecab7dab1f
kryllyxofficial01 <kryllyxofficial@gmail.com>
2026-02-19 18:23:43 -0500

    trying something different

--- src/main.py
+++ src/main.py
@@ -1,14 +1,23 @@
 import sys
 
-from playlist_loader import PlaylistLoader
+# from playlist_loader import PlaylistLoader
+
+# if len(sys.argv) < 2:
+#     print(f"Usage: python {__file__} <playlist id>")
+
+#     sys.exit(-1)
+
+# loader = PlaylistLoader()
+
+# tracks = loader.load(sys.argv[1])
+
+# print(track.track_name for track in tracks)
+
+from csv_reader import CSVReader
 
 if len(sys.argv) < 2:
-    print(f"Usage: python {__file__} <playlist id>")
+    print(f"Usage: python {__file__} <path to CSV file>")
 
-    sys.exit(-1)
+reader = CSVReader(sys.argv[1])
 
-loader = PlaylistLoader()
-
-tracks = loader.load(sys.argv[1])
-
-print(track.track_name for track in tracks)
+reader.load()
